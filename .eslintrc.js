e08676f56eeee41684b0d3b4fdb92dad71e6fd5b
Yudai Tamura <yudaitam@yahoo-corp.jp>
2023-01-31 18:00:36 +0900

    huskyの追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,9 @@ module.exports = {
     "@vue/prettier",
   ],
   overrides: [],
-  parser: "@typescript-eslint/parser",
+  parser: "vue-eslint-parser",
   parserOptions: {
+    parser: "@typescript-eslint/parser",
     ecmaVersion: "latest",
     sourceType: "module",
   },
